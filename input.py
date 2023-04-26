68722fcf87edafb4f52813fdd606b816fc2095b8
Rahul <4everhbkrahul@gmail.com>
2023-04-27 00:37:02 +0530

    Learnt input function

---
+++ input.py
@@ -0,0 +1,14 @@
+a= input("Enter your name: ") 
+#This input function gives a return value as string/character hence we have to pass that into a variable.
+b= input("Your designation: ")
+c= input("Enter your age: ")
+
+d= input("Enter first number: ")
+e= input("Enter second number: ")
+
+print("\nHey Everyone! :-) ")
+print("My name is "+a)
+print("I am a "+b)
+print("I am", int(c),"years old.")
+
+print("\nAapke diye gye do numbers k jodd hain:", int(d)+int(e),"\n")
