c1b4ec08557ecaf75fcdae915d98f3e2d65d6283
Rahul <4everhbkrahul@gmail.com>
2023-04-28 00:41:05 +0530

    string & string slicing

---
+++ stringSlicing.py
@@ -0,0 +1,11 @@
+loc="Patna, Bihar"
+fruit='ORANGE'
+
+print(len(loc))
+print(loc[7:12],'P'+loc[1:5])
+
+print(fruit[-4:-1]) #-4 means length of fruit-4 & -1 means length of fruit-1
+
+#Quick 
+nm ="Sorry"
+print(nm[-4:-2]) #or
